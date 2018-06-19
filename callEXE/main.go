319f6965afdd09a0192e333ba687bd3b712fad8a
gotolang <qulang.cn@gmail.com>
2018-06-19 23:12:54 +0800

    Add code about calling window exe program

--- callEXE/main.go
+++ callEXE/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os/exec"
 	"runtime"
 )
@@ -9,6 +10,13 @@ import (
 func main() {
 	if runtime.GOOS == "windows" {
 		fmt.Println(runtime.GOOS)
+		cmd := exec.Command("C:\\Windows\\system32\\mstsc.exe") //
+
+		err := cmd.Start()
+		if err != nil {
+			log.Println(err)
+			return
+		}
 	} else if runtime.GOOS == "darwin" {
 		fmt.Println("macos")
 		cmd := exec.Command("/Applications/Postman.app", "")
