919c7a773574f6e04be9ddda5a1ade24e94b679e
gotolang <qulang.cn@gmail.com>
2018-07-07 00:01:20 +0800

    set environment variable for windows

--- setenv/env.go
+++ setenv/env.go
@@ -1,7 +1,33 @@
 package main
 
-import "log"
+import (
+	"log"
+	"os"
+	"runtime"
+
+	"golang.org/x/sys/windows/registry"
+)
 
 func main() {
 	log.Println("set windows env")
+	os.Setenv("P", "Golang") //session environment
+
+	if runtime.GOOS == "windows" {
+		//HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\Environment
+		k, err := registry.OpenKey(registry.LOCAL_MACHINE, "SYSTEM\\CurrentControlSet\\Control\\Session Manager\\Environment", registry.QUERY_VALUE)
+		if err != nil {
+			log.Println("registry open SYSTEM\\CurrentControlSet\\Control\\Session Manager\\Environment ")
+		}
+		defer k.Close()
+
+		err = k.SetStringValue("ORACLE_HOME", "C:\\oracle\\product\\10.2")
+		if err != nil {
+			log.Println("set value error")
+		}
+	} else if runtime.GOOS == "darwin" { // macos
+
+	} else { //linux
+
+	}
+
 }
